f8c73cb6a9ae65fcf956eec713e894110327f1e2
hardik170220 <panchalhardik1702@gmail.com>
2024-04-29 11:50:16 +0530

    complete my frontend part

--- food/src/components/FoodDisplay/FoodDisplay.jsx
+++ food/src/components/FoodDisplay/FoodDisplay.jsx
@@ -9,7 +9,7 @@ const FoodDisplay = ({category}) => {
 
 
   return (
-    <div className='food-display' id='food-display'>
+    <div className="food-display" id='food-display'>
         <h2>Top Dishes near you</h2>
         <div className="food-display-list">
           {food_list.map((item,index)=>{
